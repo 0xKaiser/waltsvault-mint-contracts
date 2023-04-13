54ef99282caa4d62e1079387c1ef1d52f9350291
0xKaiser <98224518+0xKaiser@users.noreply.github.com>
2023-04-13 09:24:45 +0530

    update logic

--- contracts/ReserveForMint.sol
+++ contracts/ReserveForMint.sol
@@ -9,19 +9,22 @@ import {Whitelist} from "./utils/WhiteListSigner.sol";
 contract ReserveForMint is OwnableUpgradeable, Whitelist {
 
     IERC721Upgradeable public ravendale;
+    
     enum currentstate {NOT_STARTED, STARTED, ENDED}
     currentstate public state;
     
-    uint256 public resPerRavendale;
-    uint256 public resPrice;
     address public designatedSigner;
-    
-    address[] public addrPaid;
-    address[] public addrRavendale;
+
+    uint public resPrice;
+    uint public maxResPerAddr_FCFS;
+    uint public maxResPerSpot_VL;
     
     mapping(bytes => bool) private isSignatureUsed;
-    mapping(address => uint256) private resByAddr;
-    mapping(address => bool) private isRavendaleClaimer;
+    
+    mapping(address => uint) public resByAddr_FCFS;
+    mapping(address => uint) public resByAddr_VL;
+    mapping(address => uint[]) public tokensLockedByAddr;
+    mapping(uint => address) public lockerAddrOf;
     
     function initialize (address _ravendaleAddr) external initializer {
         __Ownable_init();
@@ -30,51 +33,67 @@ contract ReserveForMint is OwnableUpgradeable, Whitelist {
         state = currentstate.NOT_STARTED;
     }
     
-    function withdraw() external onlyOwner {
-        payable(msg.sender).transfer(address(this).balance);
+    function placeOrder(
+        uint256[] calldata tokensToLock,
+        whitelist memory signature,
+        uint256 amt_VL,
+        uint256 amt_FCFS
+    ) external payable {
+        if(tokensToLock.length > 0){
+            require(state != currentstate.NOT_STARTED);
+
+            for(uint i=0; i<tokensToLock.length; i++){
+                ravendale.safeTransferFrom(msg.sender, address(this), tokensToLock[i]);
+                tokensLockedByAddr.push[tokensToLock[i]];
+                lockerAddrOf[tokensToLock[i]] = msg.sender;
+            }
+        }
+        
+        require(msg.value == (amt_FCFS + amt_VL) * resPrice);        
+        
+        if(amt_VL > 0){
+            uint maxAllowedAmt_VL = (tokensToLock.length + tokensLockedByAddr.length 
+                            + signature.spots) * maxResPerSpot_VL;
+            
+            require(state == currentstate.STARTED);
+            require(maxAllowedAmt_VL >= resByAddr_VL[msg.sender] + amt_VL);
+            
+            verifySignature(signature);
+            
+            resByAddr_VL[msg.sender] += amt_VL;
+        }
+
+        if(amt_FCFS > 0){
+            require(state == currentstate.STARTED);
+            require(maxResPerAddr_FCFS >= resByAddr_FCFS[msg.sender] + amt_FCFS);
+            
+            resByAddr_FCFS[msg.sender] += amt_FCFS;
+        }
     }
     
-    function paidReserve(whitelist memory signature, uint256 amount) external payable {
-        require(state == currentstate.STARTED, "Participation not started");
+    function claimRefund(
+        whitelist memory signature,
+        uint256 amtAllocated,
+    ) external {
+        require(state == currentstate.ENDED, "Free participation not ended");
+        
+        verifySignature(signature);
+        
+        amtUnallocated = resByAddr_VL + resByAddr_FCFS - amtAllocated;        
+        payable(msg.sender).transfer(amtUnallocated * resPrice);
+    }
+
+    function verifySignature(whitelist memory signature) internal {
         require(getSigner(signature) == designatedSigner, "Invalid signature");
         require(block.timestamp < signature.nonce + 3 minutes, "Expired Nonce");
         require(!isSignatureUsed[signature.signature], "Nonce already used");
         require(signature.userAddress == msg.sender, "Invalid user address");
-        require(signature.amountAllowed >= amount + resByAddr[msg.sender], "Invalid amount");
-        require(msg.value == entryresPrice * amount, "Invalid value");
-        require(ravendale.balanceOf(msg.sender) == 0, "Must claim first");
-        
-        isSignatureUsed[signature.signature] = true;
-        resByAddr[msg.sender] += amount;
-        addrPaid.push(msg.sender);
     }
     
-    function ravendaleReserve(uint256[] memory tokenIds) external {
-        uint256 ravendaleBalance = ravendale.balanceOf(msg.sender);
-        
-        require(state == currentstate.STARTED, "Not started");
-        require(ravendaleBalance > 0, "You don't own any Ravendale");
-        require(ravendaleBalance == tokenIds.length, "You are not locking all Ravendales");
-        resByAddr[msg.sender] += resPerRavendale * tokenIds.length;
-        
-        if(isRavendaleClaimer[msg.sender] == false){
-            addrRavendale.push(msg.sender);
-            isRavendaleClaimer[msg.sender] = true;
-        }
-        
-        for (uint256 i = 0; i < tokenIds.length; i++) {
-            ravendale.transferFrom(msg.sender, address(this), tokenIds[i]);
-        }
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
-    
-    function refund(address[] memory to, uint256[] memory val) external {
-        require(state == currentstate.ENDED, "Free participation not ended");
-        
-        for (uint256 i = 0; i < addrs.length; i++) {
-            payable(to[i]).transfer(val[i]);
-        }
-    }
-    
+
     /**
         * @dev Restock the contract with ETH for refunding users
     */
@@ -84,20 +103,16 @@ contract ReserveForMint is OwnableUpgradeable, Whitelist {
     
     // Setters
     
-    function startReservation() external onlyOwner {
+    function openReservation() external onlyOwner {
         state = currentstate.STARTED;
     }
     
-    function endReservation() external onlyOwner {
+    function closeReservation() external onlyOwner {
         state = currentstate.ENDED;
     }
     
-    function setResPrice(uint256 _resPrice) external onlyOwner {
+    function setReservationPrice(uint256 _resPrice) external onlyOwner {
         resPrice = _entryresPrice;
-    }
-    
-    function setResPerRavendale(uint256 _resPerRavendale) external onlyOwner {
-        resPerRavendale = _resPerRavendale;
     }
     
     function setDesignatedSigner(address _designatedSigner) external onlyOwner {
@@ -109,10 +124,6 @@ contract ReserveForMint is OwnableUpgradeable, Whitelist {
     }
     
     // Getters
-    
-    function getBuyers() external view returns (address[] memory) {
-        return addrPaid;
-    }
     
     /// @notice Handle the receipt of an NFT
     /// @dev The ERC721 smart contract calls this function on the recipient
