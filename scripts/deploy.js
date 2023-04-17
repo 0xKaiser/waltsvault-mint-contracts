489775fce1ec1a4b0dd72549ff2c027df3c908ad
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2023-04-17 20:17:16 +0530

    updated deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,14 +8,12 @@ async function main() {
     let mockERC721 = await MockERC721.deploy('MockERC721', 'MERC721');
     await mockERC721.deployed();
     console.log('done')
-    for (let i = 1; i < 20; i++) {
-        let tx = await mockERC721.mint("0x455217d7d192a447ea31c7584Dba9cbD84EfD973",i)
-        await tx.wait()
-    }
+    let tx = await mockERC721.mint("0x455217d7d192a447ea31c7584Dba9cbD84EfD973",20)
+    await tx.wait()
     const WaultsVault = await ethers.getContractFactory('WaltsVaultReservation');
     let waultsVault = await WaultsVault.deploy(mockERC721.address,owner.address);
     await waultsVault.deployed();
-    let tx = await waultsVault.openReservation()
+    tx = await waultsVault.openReservation()
     await tx.wait()
     console.log('done')
 }
