8952307d2932889d7b7f8f78cd63cf3431d982f4
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2023-04-26 15:57:44 +0530

    Revert "feat:erc20 minting logic added"
    This reverts commit 2f4beef95d1d1dcea438d20287236e60e80f00fb.

--- contracts/Interfaces/IMerkel.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-
-interface IMerkel is IERC20Upgradeable{
-	function mint(address to_, uint256 amount) external;
-}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const {ethers, upgrades} = require ('hardhat');
 async function main() {
     let owner;
     [owner] = await ethers.getSigners();
-    // const MockERC721 = await ethers.getContractFactory('MockERC721');
+    const MockERC721 = await ethers.getContractFactory('MockERC721');
     // let mockERC721 = await upgrades.deployProxy(MockERC721,['MockERC721', 'MERC721']);
     // await mockERC721.deployed();
     // console.log('MockERC721 deployed to:', mockERC721.address);
@@ -15,22 +15,19 @@ async function main() {
     // let tx = await mockERC721.mint("0x455217d7d192a447ea31c7584Dba9cbD84EfD973",20)
     // await tx.wait()
     const WaultsVault = await ethers.getContractFactory('WaltsVaultReservation');
-    let waultsVault = await upgrades.upgradeProxy("0xF30d71c7DDA7244842650F3D4D569eeb60C0960b",WaultsVault);
-    await waultsVault.deployed();
-    console.log('WaultsVault deployed to:', waultsVault.address);
-    await verify(waultsVault.address, [])
+    // let waultsVault = await upgrades.deployProxy(WaultsVault,["0xBE558404f27b4F525633957269F453E657e05B8A",owner.address]);
+    // await waultsVault.deployed();
     // let tx = await waultsVault.openReservation()
     // await tx.wait()
     // console.log('WaultsVault deployed to:', waultsVault.address);
     // // const waltsVault = await WaultsVault.attach("0xcaaB2f368a4B8d8A13C3ce1f88b50D084444c043")
     // tx = await waultsVault.transferOwnership("0xca191a12662c3B875c2cac2Dc7E2EF09dFa20Cf4")
     // await tx.wait()
-    // tx = await waultsVault.placeOrder([],[1,1,owner.address,owner.address],0,2, {value: ethers.utils.parseEther("0.02")});
-    // await tx.wait()
-    // console.log("Transfered ownership")
+    tx = await waultsVault.placeOrder([],[1,1,owner.address,owner.address],0,2, {value: ethers.utils.parseEther("0.02")});
+    await tx.wait()
+    console.log("Transfered ownership")
 
-    // await tx.wait()
-    console.log("Transaction hash", tx.hash)
+
     // await verify(waultsVault.address, [mockERC721.address,owner.address])
 }
 
