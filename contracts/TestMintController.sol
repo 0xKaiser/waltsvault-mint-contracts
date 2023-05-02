38fa0f59613a01657d4fa83459a343851f555641
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2023-05-02 21:04:51 +0530

    feat: final_logic

--- contracts/TestMintController.sol
+++ contracts/TestMintController.sol
@@ -8,7 +8,7 @@ import {Signer} from "./utils/Signer.sol";
 
 contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 	
-	ITestNFT public RD;
+	ITestNFT public ravendale;
 	ITestNFT public WV;
 	
 	address public TREASURY;
@@ -19,6 +19,7 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 	
 	uint16 public MAX_MINTS_PER_ADDR_PUBLIC;
 	uint16 public MAX_AMOUNT_FOR_SALE;
+	uint16 public AVAILABLE_AMOUNT_FOR_VL;
 	uint16 public SIGNATURE_VALIDITY;
 	
 	uint32 public START_TIME_RD;
@@ -27,16 +28,16 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 	uint32 public END_TIME_VL;
 	uint32 public START_TIME_PUBLIC;
 	uint32 public END_TIME_PUBLIC;
- 
+	
 	uint256 public PRICE;
 	
 	uint16 public amountSold;
 	mapping(address => uint256) public rdMintsBy;
 	mapping(address => uint256) public vlMintsBy;
 	mapping(address => uint256) public publicMintsBy;
-    mapping(address => uint256[]) private tokensLockedBy;
+	mapping(address => uint256[]) private tokensLockedBy;
 	mapping(uint256 => address) public lockerOf;
-    mapping(bytes => bool) private isSignatureUsed;
+	mapping(bytes => bool) private isSignatureUsed;
 	
 	event RavendaleClaim(address indexed _claimer, uint256 indexed _tokenId);
 	event RavendaleMint(address _minter, uint256 indexed _amount);
@@ -44,66 +45,66 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 	event PublicMint(address _minter, uint256 indexed _amount);
 	event ReleaseRavendale(address indexed _receiver, uint256 indexed _tokenId);
 	
-	
 	function initialize() external initializer {
 		__Ownable_init();
 		__Signer_init();
 		__Pausable_init();
 		
-        RD = ITestNFT(0xBeb297284e6D4E5b020e200C99E93400f21b2Cd7);
+		ravendale = ITestNFT(0xBeb297284e6D4E5b020e200C99E93400f21b2Cd7);
 		WV = ITestNFT(0xDa67FD1Cb01a81dADec6727b9D0B74a8F2Dc1437);
-                
-		TREASURY = 0x97EaE183E6CB0D192d5820494d694312bd5436b7; 
-        AUTHORISED_SIGNER = 0xA4CC419dB3F709B2E2f3f9Eb06B6cEC14DeDdDC6;
+		
+		TREASURY = 0x97EaE183E6CB0D192d5820494d694312bd5436b7;
+		AUTHORISED_SIGNER = 0xA4CC419dB3F709B2E2f3f9Eb06B6cEC14DeDdDC6;
 		SIGNATURE_VALIDITY = uint16(5 minutes);
 		
-        PRICE = 0.0928 ether;
+		PRICE = 0.0628 ether;
 		MAX_MINTS_PER_TOKEN_RD = uint8(1);
 		MAX_MINTS_PER_SPOT_VL = uint8(1);
 		MAX_MINTS_PER_ADDR_PUBLIC = uint16(2);
-		MAX_AMOUNT_FOR_SALE = uint16(5928);
+		MAX_AMOUNT_FOR_SALE = uint16(5960);
+		AVAILABLE_AMOUNT_FOR_VL = MAX_AMOUNT_FOR_SALE - uint16(928);
 		
-		START_TIME_RD = uint32(1682847540);
-        END_TIME_RD = START_TIME_RD + uint32(8 hours);
-        START_TIME_VL = START_TIME_RD;
-        END_TIME_VL = END_TIME_RD;
-        START_TIME_PUBLIC = END_TIME_VL;
-        END_TIME_PUBLIC = START_TIME_PUBLIC + uint32(100 days);
+		START_TIME_RD = uint32(1683031962);
+		END_TIME_RD = START_TIME_RD + uint32(8 hours);
+		START_TIME_VL = START_TIME_RD;
+		END_TIME_VL = END_TIME_RD;
+		START_TIME_PUBLIC = END_TIME_VL;
+		END_TIME_PUBLIC = START_TIME_PUBLIC + uint32(100 days);
 	}
 	
 	function mint(
-        uint256 amountRD,
-		uint256 amountVL,
-		uint256 amountPUBLIC,
+		uint16 amountRD,
+		uint16 amountVL,
+		uint16 amountPUBLIC,
 		uint256[] calldata tokensToLockRD,
 		signedData memory spotsDataVL
 	) external payable whenNotPaused {
-        uint256 amountTOTAL = amountRD + amountVL + amountPUBLIC;
-
-        require(PRICE * amountTOTAL == msg.value, "mint: unacceptable payment");
-
-        require(MAX_AMOUNT_FOR_SALE >= amountSold + amountTOTAL, "mint: unacceptable amount");
-
-        if(tokensToLockRD.length > 0){
-            _ravendaleMint(amountRD, tokensToLockRD);
+		uint256 amountTOTAL = amountRD + amountVL + amountPUBLIC;
+		
+		require(PRICE * amountTOTAL == msg.value, "mint: unacceptable payment");
+		
+		require(MAX_AMOUNT_FOR_SALE  >= amountSold + amountTOTAL, "mint: unacceptable amount");
+		
+		if(tokensToLockRD.length > 0){
+			_ravendaleMint(amountRD, tokensToLockRD);
 		}
 		
 		if(amountVL > 0){
-            _vaultListMint(amountVL, spotsDataVL);
+			_vaultListMint(amountVL, spotsDataVL);
 		}
 		
 		if(amountPUBLIC > 0){
-            _publicMint(amountPUBLIC);
+			_publicMint(amountPUBLIC);
 		}
-
+		
 		amountSold += uint16(amountTOTAL);
 	}
-
-
-    // ======== INTERNAL FUNCTIONS ======== //
-
+	
+	
+	// ======== INTERNAL FUNCTIONS ======== //
+	
 	function _ravendaleMint(
-		uint256 amountRD,
+		uint16 amountRD,
 		uint256[] calldata tokensToLockRD
 	) internal {
 		require(START_TIME_VL <= block.timestamp, "ravendale: sale not started");
@@ -111,7 +112,7 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 			tokensLockedBy[msg.sender].push(tokensToLockRD[i]);
 			lockerOf[tokensToLockRD[i]] = msg.sender;
 			
-			RD.safeTransferFrom(msg.sender, address(this), tokensToLockRD[i]);
+			ravendale.safeTransferFrom(msg.sender, address(this), tokensToLockRD[i]);
 			WV.safeTransferFrom(address(this), msg.sender, tokensToLockRD[i]);
 			
 			emit RavendaleClaim(msg.sender, tokensToLockRD[i]);
@@ -127,32 +128,37 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 			
 			emit RavendaleMint(msg.sender, amountRD);
 		}
+		
+		AVAILABLE_AMOUNT_FOR_VL += uint16(tokensToLockRD.length) - amountRD;
 	}
-
+	
 	function _vaultListMint(
-		uint256 amountVL,
+		uint16 amountVL,
 		signedData memory spotsDataVL
 	) internal {
 		require(START_TIME_VL <= block.timestamp, "vault list: sale not started");
 		require(block.timestamp <= END_TIME_VL, "vault list: sale over");
+		require(amountVL <= AVAILABLE_AMOUNT_FOR_VL, "vault list: unavailable amount");
 		
 		require(block.timestamp < spotsDataVL.nonce + SIGNATURE_VALIDITY, "vault list: expired nonce");
 		require(getSigner(spotsDataVL) == AUTHORISED_SIGNER, "vault list: unauthorised signer");
 		require(!isSignatureUsed[spotsDataVL.signature], "vault list: used signature");
 		require(spotsDataVL.userAddress == msg.sender, "vault list: unauthorised address");
+
 		require(MAX_MINTS_PER_SPOT_VL * spotsDataVL.allocatedSpots >= vlMintsBy[msg.sender] + amountVL, "vault list: unacceptable amount");
-	
+		
 		isSignatureUsed[spotsDataVL.signature] = true;
 		vlMintsBy[msg.sender] += amountVL;
+		AVAILABLE_AMOUNT_FOR_VL -= amountVL;
 		
 		(address[] memory receiver, uint256[] memory AmountVL) = _getArray(spotsDataVL.userAddress, amountVL);
 		WV.airdrop(receiver, AmountVL);
-	
+		
 		emit VaultListMint(msg.sender, amountVL);
 	}
 	
 	function _publicMint(
-		uint256 amountPUBLIC
+		uint16 amountPUBLIC
 	) internal {
 		require(START_TIME_PUBLIC <= block.timestamp, "public: sale not started");
 		require(END_TIME_PUBLIC >= block.timestamp, "public: sale over");
@@ -165,7 +171,6 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 		emit PublicMint(msg.sender, amountPUBLIC);
 	}
 	
-	
 	function _getArray(address userAddress, uint256 totalTokens) internal pure returns (address[] memory addressArray, uint256[] memory tokenArray) {
 		addressArray = new address[](1);
 		tokenArray = new uint256[](1);
@@ -173,7 +178,6 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 		tokenArray[0] = totalTokens;
 	}
 	
-
 	// ======== OWNER FUNCTIONS ======== //
 	
 	/**
@@ -186,19 +190,17 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 			_pause();
 		}
 	}
-        
+	
 	function releaseRavendale(
 		address[] calldata lockers
 	) external onlyOwner {
 		for(uint256 j=0; j<lockers.length; j++){
 			uint256[] memory tokensToRelease = tokensLockedBy[lockers[j]];
-			
-				for(uint256 i=0; i<tokensToRelease.length; i++){
-					lockerOf[tokensToRelease[i]] = address(0);
-					delete tokensLockedBy[lockers[j]];
-					RD.safeTransferFrom(address(this), lockers[j], tokensToRelease[i]);
-					
-					emit ReleaseRavendale(lockers[j], tokensToRelease[i]);
+			delete tokensLockedBy[lockers[j]];
+			for(uint256 i=0; i<tokensToRelease.length; i++){
+				lockerOf[tokensToRelease[i]] = address(0);
+				ravendale.safeTransferFrom(address(this), lockers[j], tokensToRelease[i]);
+				emit ReleaseRavendale(lockers[j], tokensToRelease[i]);
 			}
 		}
 	}
@@ -207,13 +209,13 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 		payable(TREASURY).transfer(address(this).balance);
 	}
 	
-    function setRavendaleAddr(address _ravendale) external onlyOwner {
-		RD = ITestNFT(_ravendale);
+	function setRavendaleAddr(address _ravendale) external onlyOwner {
+		ravendale = ITestNFT(_ravendale);
 	}
-
-    function setWaltsVaultAddr(address _waltsVault) external onlyOwner {
-        WV = ITestNFT(_waltsVault);
-    }
+	
+	function setWaltsVaultAddr(address _WV) external onlyOwner {
+		WV = ITestNFT(_WV);
+	}
 	
 	function setTreasury(address _treasury) external onlyOwner {
 		TREASURY = _treasury;
@@ -264,7 +266,7 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 	}
 	
 	// ======== READ FUNCTIONS ======== //
-
+	
 	function getTokensLockedByAddr(address addr) external view returns(uint256[] memory){
 		return tokensLockedBy[addr];
 	}
@@ -277,9 +279,9 @@ contract TestMintController is OwnableUpgradeable, Signer, PausableUpgradeable {
 		return tokensLockedBy[addr][index];
 	}
 	
-
+	
 	// ======== AUXILIARY FUNCTIONS ======== //
-
+	
 	function onERC721Received(
 		address,
 		address,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,31 +4,23 @@ async function main() {
     let owner;
     [owner] = await ethers.getSigners();
 
-    // const Test = await ethers.getContractFactory('TestToken300423');
-    // const test = await upgrades.deployProxy(Test, ['TestToken300423', 'TST']);
-    // await test.deployed();
-    // console.log('test deployed at:', test.address);
-    //
-    const WaltsVault = await ethers.getContractFactory('TestMintController');
-    // const waltsVault = await upgrades.deployProxy(WaltsVault, ['TestVault', 'TV']);
-    const wault = await WaltsVault.attach('0xDa67FD1Cb01a81dADec6727b9D0B74a8F2Dc1437');
-    let tx = await wault.transferOwnership("0x75dCAb9bDBEe7b907441d517d56491EA6DCBd608");
-    // let tx = await wault.toggleController(owner.address);
-    await tx.wait()
-    // tx = await wault.airdrop(["0xE4B1903789c0CCE595A47A1dC921874DcF656F5f"],[20])
-    // await tx.wait()
-    // console.log('wault deployed at:', wault.address);
-    // await waltsVault.deployed();
-    // console.log('waltsVault deployed at:', waltsVault.address);
+    const Test = await ethers.getContractFactory('TestToken300423');
+    const test = await upgrades.deployProxy(Test, ['Test RD', 'T RD']);
+    await test.deployed();
+    console.log('test deployed at:', test.address);
 
-    // const MintController = await ethers.getContractFactory('TestMintController');
-    // const mintController = await upgrades.deployProxy(MintController, []);
-    // await mintController.deployed();
+    const WaltsVault = await ethers.getContractFactory('TestVault');
+    const waltsVault = await upgrades.deployProxy(WaltsVault, ['Test WV', 'T WV']);
+    await waltsVault.deployed();
+    console.log('waltsVault deployed at:', waltsVault.address);
 
-    console.log('---------------------------')
-    console.log('deployer address: ', owner.address);
-    // console.log('MintController deployed at:', mintController.address);
-    // console.log('---------------------------')
+    const MintController = await ethers.getContractFactory('TestMintController');
+    const mintController = await upgrades.deployProxy(MintController, []);
+    await mintController.deployed();
+    console.log('mintController deployed at:', mintController.address);
+
+    await verify(mintController.address, []);
+
 }
 
 async function verify (contractAddress, args) {
