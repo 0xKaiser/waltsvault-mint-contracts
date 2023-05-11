61521ddea5ac6e722deedf0395d3a0f386bb80cf
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2023-05-11 14:21:31 +0530

    Update upgrade.js

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -5,8 +5,10 @@ async function main() {
     [owner] = await ethers.getSigners();
 
     const MintController = await ethers.getContractFactory("WaltsVaultMintController");
-    const mintController = await upgrades.upgradeProxy('0xf9ae821914097DFb8C23D27aA2ECfC22C279aFDc', MintController);
-    await mintController.deployed();
+    // const mintController = await upgrades.upgradeProxy('0xf9ae821914097DFb8C23D27aA2ECfC22C279aFDc', MintController);
+    const mintController = await MintController.attach('0xf9ae821914097DFb8C23D27aA2ECfC22C279aFDc');
+    // await mintController.deployed();
+    console.log("Available Amount For VL", await mintController.AVAILABLE_AMOUNT_FOR_VL());
     console.log('Order deployed to:', mintController.address);
 }
 main()
